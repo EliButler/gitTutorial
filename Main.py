e5f61bb6d8ad88e84ac4defbbd5a0870b7c400e1
EliButler <40725211+EliButler@users.noreply.github.com>
2020-12-04 09:58:17 -0600

    Changed Main.py

--- Main.py
+++ Main.py
@@ -4,11 +4,11 @@ def p_line():
     """Print a line"""
     print('If this is working, you should be able to read this!')
 
-def P_line_2():
+def P_line_New():
     """Prints a second Line"""
     print('This should be on a second line!')
 
 
 p_line()
-P_line_2()
+P_line_New()
 
